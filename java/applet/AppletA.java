becabccdda391f48b8d8ca6e9cda58a01297f8c0
zhaolei03 <zhaolei03@baidu.com>
2013-08-30 15:37:06 +0800

    java hadoop

---
+++ java/applet/AppletA.java
@@ -0,0 +1,14 @@
+import java.awr.*;
+import java.applet.*;
+
+public class HelloApplet extends Applet { //an applet
+
+    public void paint(Graphics g){
+
+        g.drawString ("Hello World!",20,20);
+
+    }
+
+}
+
+
