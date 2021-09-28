9a0ea45c96a4ae5620887cdab8d5af2a351b6b88
Zhao Lei <zhaolei@i1602.com>
2021-09-28 20:00:24 +0800

    Delete java directory

--- java/hadoop/wordcount/w.sh
+++
@@ -1,4 +0,0 @@
-javac -classpath /stone/ser/hadoop-1.2.1/hadoop-core-1.2.1.jar -d wordcount_classes WordCount.java
-jar -cvf wordcount.jar -C wordcount_classes/ .
-bin/hadoop jar /usr/joe/wordcount.jar org.myorg.WordCount /usr/joe/wordcount/input /usr/joe/wordcount/output
-
--- java/hello/HelloWorld.java
+++
@@ -1,6 +0,0 @@
-package org.stonex;
-public class HelloWorld{ 
-    public static void main(String[] args) {  
-        System.out.println("Hello World!"); 
-    }
-}
